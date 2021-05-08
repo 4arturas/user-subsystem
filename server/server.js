a8338f48c4e2bf2e09d8794daaf153693a72d654
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2021-05-08 08:50:36 +0300

    Use express server to supply front end to user

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
-console.log( 'Hello' );
+const express           = require('express');
+const path              = require('path');
+
+const app               = express();
+
+const pathToStaticResources = '../app/dist'
+app.use('/assets', express.static(path.join( __dirname, pathToStaticResources + '/assets' )));
+app.use('/img', express.static(path.join( __dirname, pathToStaticResources + '/img' )));
+
+app.get('/', (req, res) => {
+    const htmlFile = path.join( __dirname, pathToStaticResources ) + '/index.html';
+    res.sendFile( htmlFile );
+})
+
+const USERS_APP_PORT = 4000;
+app.listen( USERS_APP_PORT );
+console.log(`Running on port ${USERS_APP_PORT}`);
