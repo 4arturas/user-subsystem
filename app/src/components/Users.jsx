9652264fed102c3fc4e73c3c46dc23cebf5c092c
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2021-05-08 14:31:46 +0300

    Add Users.jsx component

---
+++ app/src/components/Users.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import '../App.css'
+import {
+    BrowserRouter,
+    Route,
+    Switch,
+    useHistory,
+    useLocation
+}
+    from "react-router-dom";
+
+function Users() {
+
+    return (
+        <div>Users</div>
+    )
+}
+
+export default Users
