47d0d5d6630dd788d8c7ac173e05899f0281d773
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2021-05-08 14:32:52 +0300

    Add Clients.jsx component

---
+++ app/src/components/Clients.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import '../App.css'
+import {
+    BrowserRouter,
+    Route,
+    Switch,
+    useHistory,
+    useLocation
+}
+    from "react-router-dom";
+
+function Clients() {
+
+    return (
+        <div>Clients</div>
+    )
+}
+
+export default Clients
