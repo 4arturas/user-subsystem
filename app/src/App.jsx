56673dd031d0e2e6a607cc426a7e38c2274d6a8b
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2021-05-08 18:45:36 +0300

    Update UI design

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './App.css'
 import './header.css'
 import {
@@ -11,16 +11,19 @@ import {
     from "react-router-dom";
 import Home from "./components/Home";
 import Organizations from "./components/Organizations";
+import Users from "./components/Users";
+import Roles from "./components/Roles";
+import Clients from "./components/Clients";
 
-function App() {
-    const [count, setCount] = useState(0)
+function App()
+{
 
     return (
         <BrowserRouter>
-            <div className="header">
+            <div className="header" style={{height:'20px'}}>
                 <div className='header__left'>
                     <NavLink to="/">
-                        <img style={{width:'30px'}} src={'img/logo.png'} alt="Logo"/>
+                        <img style={{width:'90px', position: 'absolute', top:'-5px'}} src={'img/logo.png'} alt="Logo"/>
                     </NavLink>
                 </div>
                 <div className="header__center">
@@ -30,12 +33,30 @@ function App() {
                     <NavLink to="/organizations" className="header__option" activeClassName="header__option--active">
                         Organizations
                     </NavLink>
+                    <NavLink to="/users" className="header__option" activeClassName="header__option--active">
+                        Users
+                    </NavLink>
+                    <NavLink to="/roles" className="header__option" activeClassName="header__option--active">
+                        Roles
+                    </NavLink>
+                    <NavLink to="/clients" className="header__option" activeClassName="header__option--active">
+                        Clients
+                    </NavLink>
                 </div>
             </div>
             <div style={{textAlign:'center'}}>
                 <Switch>
                     <Route exact path="/organizations">
                         <Organizations/>
+                    </Route>
+                    <Route exact path="/users">
+                        <Users/>
+                    </Route>
+                    <Route exact path="/roles">
+                        <Roles/>
+                    </Route>
+                    <Route exact path="/clients">
+                        <Clients/>
                     </Route>
                     <Route exact path={["/","/home"]}>
                         <Home/>
