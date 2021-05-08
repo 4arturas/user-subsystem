3048af29cf1672b1776492d6de2a0321f8ce450d
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2021-05-08 14:30:55 +0300

    Update Organizations.jsx with preliminary thoughts

--- app/src/components/Organizations.jsx
+++ app/src/components/Organizations.jsx
@@ -1,18 +1,56 @@
-import React, {useState} from 'react'
-import '../App.css'
-import {
-    BrowserRouter,
-    Route,
-    Switch,
-    useHistory,
-    useLocation
+import React from 'react'
+import {Button, Dialog, Input} from "@material-ui/core";
+
+
+function AddOrganization()
+{
+    const [open, setOpen] = React.useState(false);
+    const handleClickOpen = async () =>
+    {
+        setOpen(true);
+    };
+
+    const handleClose = (value) =>
+    {
+        setOpen(false);
+    };
+
+    return (
+        <span>
+        <Button variant="contained" color="primary" onClick={() => { handleClickOpen(); }}>
+            Add new organizaton
+        </Button>
+        <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open} maxWidth="xl">
+            <div style={{padding: '5px'}}>
+                <div style={{borderBottom: '1px solid gray', fontWeight: 'bold', fontStyle: 'italic', fontSize: 'large'}}>
+                    Add new organization
+                </div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Organization name:</td>
+                            <td><Input/></td>
+                        </tr>
+                        <tr>
+                            <td colSpan={2}>
+                                <Button>Add organization</Button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </Dialog>
+        </span>
+    );
 }
-    from "react-router-dom";
 
 function Organizations() {
 
     return (
-        <div>Organizations</div>
+        <div>
+            <div>Organizations</div>
+            <AddOrganization/>
+        </div>
     )
 }
 
