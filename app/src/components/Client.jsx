cf0c2369255d36ba6b30a13a7a60d0c9546dc7ef
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2021-06-09 13:25:50 +0300

    Show organizations with attach, detach to client functionality

--- app/src/components/Client.jsx
+++ app/src/components/Client.jsx
@@ -9,6 +9,12 @@ import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import SearchReplace from "./SearchReplace";
 
+const headCells = [
+    { id: 'org_id', numeric: false, disablePadding: false, label: 'org_id' },
+    { id: 'org_name', numeric: false, disablePadding: false, label: 'Organization Name' },
+    { id: 'org_add_date', numeric: false, disablePadding: false, label: 'Action' }
+];
+
 function ClientChild( {client} )
 {
     return (
@@ -26,110 +32,68 @@ function ClientChild( {client} )
         </table>
     );
 }
-function OrganizationsRow( {row, searchValue, rowComponentExt1/*operation*/, rowComponentExt2} )
+
+
+function OrganizationsRow( { row, searchValue, rowComponentExt1 } )
 {
-    const operation = ( org_id ) =>
-    {
-        const operation = rowComponentExt1;
-        const clientId  = rowComponentExt2;
-        let jSonResult;
-        if ( operation === OPERATION_SHOW )
-            jSonResult = API.detach_ClientFromOrganization( clientId, org_id );
-        else
-            jSonResult = API.attach_ClientToOrganization( clientId, org_id );
-    }
+    const [organizationBelongsToClient, setOrganizationBelongsToClient]   = React.useState( 0 );
+    const [operation, setOperation] = React.useState( false );
+
+    React.useEffect( () => {
+        setOrganizationBelongsToClient( row.belongs );
+    }, [] );
+
     return (
         <TableRow
             hover
             tabIndex={-1}
             key={row.org_id}>
             <TableCell align="left">{row.org_id}</TableCell>
-            <TableCell align="left"><SearchReplace value={row.org_name} markValue={searchValue}/></TableCell>
-            <TableCell align="left">
-                <Button variant="contained" color="primary" onClick={() => { operation( row.org_id); }}>
-                    {rowComponentExt1===OPERATION_SHOW?<>Detach</>:<>Attach</>}
-                </Button>
+            <TableCell align="left" style={{whiteSpace:'nowrap'}}><SearchReplace value={row.org_name} markValue={searchValue}/></TableCell>
+            <TableCell align="center">
+                { operation ? <CircularProgress size={30}/> :
+                    organizationBelongsToClient ?
+                    <Button variant="contained" color="primary" onClick={ async () => {
+                        setOperation( true );
+                        const clientId = rowComponentExt1;
+                        const jSonResult = await API.detach_ClientFromOrganization( clientId, row.org_id );
+                        console.log( jSonResult );
+                        setOrganizationBelongsToClient( 0 );
+                        setOperation( false );
+                    } }>
+                        Detach
+                    </Button>
+                   :
+                    <Button variant="contained" color="secondary" onClick={ async () => {
+                        setOperation( true );
+                        const clientId = rowComponentExt1;
+                        const jSonResult = await API.attach_ClientToOrganization( clientId, row.org_id );
+                        console.log( jSonResult );
+                        setOrganizationBelongsToClient( 1 );
+                        setOperation( false );
+                    }} >
+                        Attach
+                    </Button>
+                }
             </TableCell>
         </TableRow>
     );
 }
 
-const OPERATION_SHOW        = 0;
-const OPERATION_ATTACH      = 1;
-
-function ShowOrganizations( { operation, clientId } )
-{
-    const headCells = [
-        { id: 'org_id', numeric: false, disablePadding: false, label: 'org_id' },
-        { id: 'org_name', numeric: false, disablePadding: false, label: 'Organization Name' },
-        { id: 'org_add_date', numeric: false, disablePadding: false, label: 'Action' }
-    ];
-
-    const [open, setOpen]   = React.useState(false);
-    const [data, setData]   = React.useState( null );
-
-    const handleClickOpen = async () =>
-    {
-        setOpen(true);
-        let organizations;
-        if ( operation === OPERATION_SHOW )
-        {
-            organizations = await API.get_OrganizationsByClient(clientId);
-            setData( organizations );
-        }
-        else
-        {
-            organizations = await API.get_OrganizationsNotBelongingToClient(clientId);
-            const newOrganizations = [];
-            for ( let i = 0; i < organizations.length; i++ )
-            {
-                const organization = organizations[i];
-                // if ( organizations.end_date !== null )
-                //     continue;
-                newOrganizations.push( organization );
-            } // end for i
-            setData( newOrganizations );
-        }
-
-    };
-
-    const handleClose = (value) =>
-    {
-        setOpen(false);
-    };
-
-    return (
-        <>
-            { operation === OPERATION_SHOW ?
-                <>
-                    <Button variant="contained" color="primary" onClick={() => { handleClickOpen(); }}>
-                        Show
-                    </Button> organizations belonging to client
-                </> :
-                <>
-                    <Button variant="contained" color="primary" onClick={() => { handleClickOpen(); }}>
-                        Show
-                    </Button> organizations <strong>not</strong> belonging to client
-                </>
-            }
-        <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open} maxWidth="xl" style={{width:'80%',height:'80%'}}>
-            <div style={{padding: '20px'}}>
-                <div>{ !data ? <><CircularProgress size={15}/>&nbsp;&nbsp;&nbsp;Wait a second ...</> : <CommonTable headCells={headCells} data={ data } RowComponent={ OrganizationsRow } rowComponentExt1={operation} rowComponentExt2={clientId} />}</div>
-            </div>
-        </Dialog>
-        </>
-    );
-}
 
 function Client()
 {
     const [client, setClient] = React.useState(null);
+    const [organizations, setOrganizations] = React.useState(null);
     const query = new URLSearchParams(window.location.search);
     const id = query.get('id');
     React.useEffect(async ()=>
     {
         const data = await API.get_Client( id );
         setClient( data );
+
+        const organizationsData = await API.get_OrganizationsWithBelongInfo( id );
+        setOrganizations( organizationsData );
     }, [] );
 
     return (
@@ -141,10 +105,15 @@ function Client()
                 <table>
                     <tbody>
                     <tr>
-                        <td><ShowOrganizations operation={OPERATION_SHOW} clientId={client.client_id}/></td>
-                    </tr>
-                    <tr>
-                        <td><ShowOrganizations operation={OPERATION_ATTACH} clientId={client.client_id}/></td>
+                        <td style={{width:'50%'}}></td>
+                        <td>
+                            {
+                                organizations === null ?
+                                    <div>Loading...</div> :
+                                    <CommonTable headCells={headCells} data={ organizations } RowComponent={ OrganizationsRow } rowComponentExt1={id} />
+                            }
+                        </td>
+                        <td style={{width:'50%'}}></td>
                     </tr>
                     </tbody>
                 </table>
@@ -152,6 +121,6 @@ function Client()
             }
         </div>
     );
-}
+};
 
 export default Client
