e8374b6a8389d7eafaf0ade806926af131a878d1
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2021-06-10 14:54:12 +0300

    Update Users.jsx design

--- app/src/components/Users.jsx
+++ app/src/components/Users.jsx
@@ -242,14 +242,14 @@ function UserRow( {row, searchValue} )
                   tabIndex={-1}
                   key={row.user_id}
         >
-            <TableCell align="left">
+            <TableCell align="left" style={{whiteSpace:'nowrap'}}>
                 <NavLink to={"/users/user?id=" + row.user_id}>
                     <SearchReplace value={row.user_name} markValue={searchValue}/>
                 </NavLink>
             </TableCell>
-            <TableCell align="left"><SearchReplace value={row.first_name} markValue={searchValue}/></TableCell>
-            <TableCell align="left"><SearchReplace value={row.last_name} markValue={searchValue}/></TableCell>
-            <TableCell align="left"><SearchReplace value={user_add_date} markValue={searchValue}/></TableCell>
+            <TableCell align="left" style={{whiteSpace:'nowrap'}}><SearchReplace value={row.first_name} markValue={searchValue}/></TableCell>
+            <TableCell align="left" style={{whiteSpace:'nowrap'}}><SearchReplace value={row.last_name} markValue={searchValue}/></TableCell>
+            <TableCell align="left" style={{whiteSpace:'nowrap'}}><SearchReplace value={user_add_date} markValue={searchValue}/></TableCell>
         </TableRow>
     );
 }
