bd4bd52b87f3d2f5fff1dc83890e78b06fd68c35
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2021-06-10 13:07:01 +0300

    Remove AddOrganization from Organizations.jsx

--- app/src/components/Organizations.jsx
+++ app/src/components/Organizations.jsx
@@ -8,48 +8,6 @@ import SearchReplace from "./SearchReplace";
 import CommonTable from "./CommonTable";
 import * as GB from "../global";
 
-function AddOrganization()
-{
-    const [open, setOpen] = React.useState(false);
-    const handleClickOpen = async () =>
-    {
-        setOpen(true);
-    };
-
-    const handleClose = (value) =>
-    {
-        setOpen(false);
-    };
-
-    return (
-        <span>
-        <Button variant="contained" color="primary" onClick={() => { handleClickOpen(); }}>
-            Add new organization
-        </Button>
-        <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open} maxWidth="xl">
-            <div style={{padding: '5px'}}>
-                <div style={{borderBottom: '1px solid gray', fontWeight: 'bold', fontStyle: 'italic', fontSize: 'large'}}>
-                    Add new organization
-                </div>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>Organization name:</td>
-                            <td><Input/></td>
-                        </tr>
-                        <tr>
-                            <td colSpan={2}>
-                                <Button>Add organization</Button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </Dialog>
-        </span>
-    );
-}
-
 function OrganizationsRow( {row, searchValue} )
 {
     const org_add_date = GB.format_Date1(row.org_add_date);
