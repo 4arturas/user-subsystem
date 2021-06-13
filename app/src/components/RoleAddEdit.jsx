7f9b340562af63a9f3747c4c0a4c0fa2617273ca
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2021-06-13 08:17:19 +0300

    Add RoleAddEdit.jsx

---
+++ app/src/components/RoleAddEdit.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Button, CircularProgress, Dialog, TextField} from "@material-ui/core";
+import API from "../API";
+import {Alert} from "@material-ui/lab";
+import {Edit} from "@material-ui/icons";
+
+export default function RoleAddEdit( { id, name } )
+{
+    const refButton                                 = React.useRef();
+    const refCancel                                 = React.useRef();
+    const refClose                                  = React.useRef();
+    const refMsg                                    = React.useRef();
+    const [open, setOpen]                           = React.useState(false);
+    const [roleName, setRoleName ]                  = React.useState('');
+    const [errorRoleName, setErrorRoleName]         = React.useState('');
+    React.useEffect( () => {
+        if ( id )
+            setRoleName( name );
+    }, [] );
+    const validateRoleName = ( value ) =>
+    {
+        if ( value.length === 0 )
+        {
+            setErrorRoleName( 'More than 0 symbols' );
+            return false;
+        }
+        else if ( value.length > 11 )
+        {
+            setErrorRoleName( 'Less or equal 11 to symbols' );
+            return false;
+        }
+        else
+        {
+            setErrorRoleName( '' );
+            return true
+        }
+    }
+    return <span>
+        { id ?
+            <Edit style={{cursor:'pointer'}} onClick={ ()=>{ setOpen( true ); } } /> :
+            <Button variant="contained" color="primary" onClick={()=>{ setOpen( true ); }}>Add New Role</Button>
+        }
+        <Dialog onClose={()=>setOpen(false)} aria-labelledby="simple-dialog-title" open={open} maxWidth="xl"
+                disableBackdropClick
+                disableEscapeKeyDown>
+            <div style={{padding: '20px'}}>
+                <div style={{borderBottom: '1px solid gray', fontWeight: 'bold', fontStyle: 'italic', fontSize: 'large'}}>
+                    { id ? 'Edit role' : 'Add new role' }
+                </div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <TextField label='Role Name'
+                                           helperText={ errorRoleName }
+                                           error={ errorRoleName.length > 0 }
+                                           onChange={ (event) => {
+                                               const value = event.target.value;
+                                               setRoleName( value );
+                                               validateRoleName( value );
+                                           } }
+                                           required
+                                           autoComplete="off"
+                                           defaultValue={ name ? name : '' }
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <Button ref={refButton} variant="contained" color="primary" onClick={ async () => {
+                                    const valid = validateRoleName( roleName );
+                                    if ( !valid )
+                                    {
+                                        ReactDOM.render(
+                                            <Alert severity="error">Fill all required values</Alert>
+                                            ,refMsg.current );
+                                        return;
+                                    }
+                                    refButton.current.style.display = 'none';
+                                    refCancel.current.style.display = 'none';
+                                    refClose.current.style.display = 'none';
+                                    ReactDOM.render(
+                                        <Alert severity="info"><CircularProgress size={15}/>&nbsp;&nbsp;&nbsp;Wait a second ...</Alert>
+                                        ,refMsg.current );
+
+                                    let jSonResponse;
+                                    if ( id )
+                                        jSonResponse = await API.update_Role( id, roleName );
+                                    else
+                                        jSonResponse = await API.add_Role( roleName );
+
+                                    if ( jSonResponse.hasOwnProperty('error') )
+                                    {
+                                        refButton.current.style.display = '';
+                                        refCancel.current.style.display = '';
+                                        refClose.current.style.display = 'none';
+                                        ReactDOM.render(
+                                            <Alert severity="error">{jSonResponse.error}</Alert>
+                                            ,refMsg.current );
+                                        return;
+                                    }
+
+                                    refButton.current.style.display = 'none';
+                                    refCancel.current.style.display = 'none';
+                                    refClose.current.style.display = '';
+                                    ReactDOM.render(
+                                        <Alert severity="success">Role was successfully {id?'updated':'added'}</Alert>
+                                        ,refMsg.current );
+
+                                }}>{ id ? 'Edit Role' : 'Add Role' }</Button>
+                                &nbsp;&nbsp;&nbsp;
+                                <Button ref={refCancel} variant="contained" color="primary" onClick={ () => { setOpen(false ); } }>Cancel</Button>
+                                <Button ref={refClose} style={{display:'none'}} variant="contained" color="primary" onClick={ () => { window.location = id ? `/roles/role?id=${id}` : `/roles` } }>Close</Button>
+                                <div ref={refMsg}></div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </Dialog>
+    </span>;
+}
